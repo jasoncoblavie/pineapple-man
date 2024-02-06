09698c72d4842f9ba47f8edec3073d615328fc80
Daniel Glover <iamglover02@gmail.com>
2024-02-06 23:15:16 +0000

    updates #2

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ yesBtn.addEventListener('click', () => {
 });
 
 noBtn.addEventListener('mouseover', ()=> {
+
     const noBtnRect = noBtn.getBoundingClientRect();
     const maxX = window.innerWidth - noBtnRect.width;
     const maxY = window.innerHeight - noBtnRect.height;
@@ -18,6 +19,12 @@ noBtn.addEventListener('mouseover', ()=> {
     const randomX = Math.floor(Math.random() * maxX);
     const randomY = Math.floor(Math.random() * maxY);
 
+    noBtn.style.position = 'absolute';
     noBtn.style.left = randomX + 'px';
     noBtn.style.top = randomY + 'px';
 })
+
+gsap.to('.question', {
+    x: 30,
+    duration: 5,
+});
