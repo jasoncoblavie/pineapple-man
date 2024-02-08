a25af20d3452311b802b48626896ad1681dc2d54
Daniel Glover <iamglover02@gmail.com>
2024-02-08 00:14:52 +0000

    updates #@

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
-const wrapper = document.querySelector('wrapper');
-const question = document.querySelector('question');
-const gif = document.querySelector('.gif');
-const yesBtn = document.querySelector('.yes-btn');
-const noBtn = document.querySelector('.no-btn');
+const wrapper = document.querySelector("wrapper");
+const question = document.querySelector("question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+import gsap from "gsap/gsap-core";
 
-yesBtn.addEventListener('click', () => {
-    question.innerHTML = "I love you too!";
-    gif.src = 
-    "https://www.icegif.com/bear-kissing-2/"
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "I love you too!";
+  gif.src = "https://www.icegif.com/bear-kissing-2/";
 });
 
-noBtn.addEventListener('mouseover', ()=> {
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
 
-    const noBtnRect = noBtn.getBoundingClientRect();
-    const maxX = window.innerWidth - noBtnRect.width;
-    const maxY = window.innerHeight - noBtnRect.height;
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
 
-    const randomX = Math.floor(Math.random() * maxX);
-    const randomY = Math.floor(Math.random() * maxY);
-
-    noBtn.style.position = 'absolute';
-    noBtn.style.left = randomX + 'px';
-    noBtn.style.top = randomY + 'px';
-})
-
-gsap.to('.question', {
-    x: 30,
-    duration: 5,
+  noBtn.style.position = "absolute";
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
 });
+
+//rotate and move elements with a class of "box" ("x" is a shortcut for a translateX() transform) over the course of 1 second.
+gsap.to(".question", { rotation: 27, x: 100, duration: 1 });
