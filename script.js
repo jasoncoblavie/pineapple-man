de106386e82e2d962ce9e7a9576a2b37c0a9577f
jasoncoblavie <125926124+jasoncoblavie@users.noreply.github.com>
2024-02-12 14:54:49 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
-const wrapper = document.querySelector("wrapper");
-const question = document.querySelector("question");
-const gif = document.querySelector(".gif");
-const yesBtn = document.querySelector(".yes-btn");
-const noBtn = document.querySelector(".no-btn");
-import gsap from "gsap/gsap-core";
+const wrapper = document.querySelector('wrapper');
+const question = document.querySelector('question');
+const gif = document.querySelector('.gif');
+const yesBtn = document.querySelector('.yes-btn');
+const noBtn = document.querySelector('.no-btn');
 
-yesBtn.addEventListener("click", () => {
-  question.innerHTML = "I love you too!";
-  gif.src = "https://www.icegif.com/bear-kissing-2/";
+yesBtn.addEventListener('click', () => {
+    question.innerHTML = "I love you too!";
+    gif.src = 
+    "https://www.icegif.com/bear-kissing-2/"
 });
 
-noBtn.addEventListener("mouseover", () => {
-  const noBtnRect = noBtn.getBoundingClientRect();
-  const maxX = window.innerWidth - noBtnRect.width;
-  const maxY = window.innerHeight - noBtnRect.height;
+noBtn.addEventListener('mouseover', ()=> {
+    const noBtnRect = noBtn.getBoundingClientRect();
+    const maxX = window.innerWidth - noBtnRect.width;
+    const maxY = window.innerHeight - noBtnRect.height;
 
-  const randomX = Math.floor(Math.random() * maxX);
-  const randomY = Math.floor(Math.random() * maxY);
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * maxY);
 
-  noBtn.style.position = "absolute";
-  noBtn.style.left = randomX + "px";
-  noBtn.style.top = randomY + "px";
-});
-
-//rotate and move elements with a class of "box" ("x" is a shortcut for a translateX() transform) over the course of 1 second.
+    noBtn.style.left = randomX + 'px';
+    noBtn.style.top = randomY + 'px';
+})
